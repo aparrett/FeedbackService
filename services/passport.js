a570aca1ad1df12fa92ebd4e05531d9a5adf4946
Anthony Parrett <anthonyparrett7@gmail.com>
2018-06-29 18:39:46 -0500

    Set up cookie authentication

--- services/passport.js
+++ services/passport.js
@@ -5,6 +5,16 @@ const keys = require('../config/keys');
 
 const User = mongoose.model('users');
 
+passport.serializeUser((user, done) => {
+  done(null, user.id);
+});
+
+passport.deserializeUser((id, done) => {
+  User.findById(id).then(user => {
+    done(null, user);
+  });
+});
+
 passport.use(
   new GoogleStrategy({
     clientID: keys.googleClientID,
@@ -12,7 +22,12 @@ passport.use(
     callbackURL: '/auth/google/callback'
   }, 
   (accessToken, refreshToken, profile, done) => {
-    console.log(profile)
-    new User({ googleId: profile.id }).save();
+    User.findOne({ googleId: profile.id }).then((existingUser) => {
+      if (existingUser) {
+        done(null, existingUser);
+      } else {
+        new User({ googleId: profile.id }).save().then(user => done(null, user));
+      }
+    });
   })
 );
